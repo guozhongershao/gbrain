6bf98857b064011bab3267ead4eadf9a50a7fe2b
guozhongershao <810018022@qq.com>
2019-01-20 02:09:44 +0800

    1. 完成nav-trigger 按钮组件；

--- src/components/widgets/NavTrigger.vue
+++ src/components/widgets/NavTrigger.vue
@@ -1,8 +1,15 @@
+<!--
+  nav 菜单trigger 按钮，
+  提供用户自定义属性：
+  size ; 按钮大小
+  backgroundColor : 背景颜色
+  tintColor : 按钮里横线的颜色
+-->
 <template>
-  <button class="nav-trigger" :class="{'nav-active' : isNavActive}" @click="toggleNavActive()">
-    <span></span>
-    <span></span>
-    <span></span>
+  <button :style="customerStyle" class="nav-trigger" :class="{'nav-active' : isNavActive}" @click="toggleNavActive()">
+    <span :style="{'background-color' : '#' + tintColor}"></span>
+    <span :style="{'background-color' : '#' + tintColor}"></span>
+    <span :style="{'background-color' : '#' + tintColor}"></span>
   </button>
 </template>
 
@@ -11,21 +18,32 @@
     name: "NavTrigger",
     data() {
       return {
-        baseSize: 10, // 默认大小 10px
         isNavActive: false,
+        customerStyle: {
+          // font-size 最小12px 原因未知，放大100倍，处理小于12px的尺寸
+          'font-size': `${this.size * 100}px`,
+          'background-color': `#${this.backgroundColor}`
+        }
       }
     },
     props: {
       size: {
-        default: 10,
+        default: 100, // 默认100px
+      },
+      backgroundColor: {
+        default: 'ffffff',
+        type: [String],
+      },
+      tintColor: {
+        default: '000000'
       }
     },
     methods: {
-      toggleNavActive(){
+      toggleNavActive() {
         console.log('active');
-        if (this.isNavActive){
+        if (this.isNavActive) {
           this.isNavActive = false;
-        }else{
+        } else {
           this.isNavActive = true;
         }
       }
@@ -36,37 +54,57 @@
 <style scoped>
   .nav-trigger {
     display: inline-block;
-    width: 3.5rem;
-    height: 2.9rem;
-    padding: 0;
+    width: 0.01em;
+    height: 0.01em;
     margin: 0;
     border: 0;
+    /* outline : none； 解决button有边框的问题 */
+    outline: none;
+    /* 解决背景颜色*/
     background-color: transparent;
-    padding: 0 .2rem;
+    padding: 0 .00063em;
+    transition: all .4s ease;
   }
 
   .nav-trigger span {
     display: block;
     width: 100%;
-    height: 0.4rem;
+    height: 0.00126em;
     background-color: black;
     float: right;
-    transition: all 0.4s ease;
-    border-radius: 0.3rem;
+    border-radius: 0.00095em;
     box-sizing: content-box;
+    transition: all .3s ease;
+    transform-origin: center center;
   }
 
   .nav-trigger span + span {
-    margin-top: 5px;
+    margin-top: 0.00157em;
   }
 
   .nav-trigger span:last-child {
     width: 75%;
   }
 
-  .nav-trigger:hover span, .nav-trigger:active span {
+  .nav-trigger:hover span, .nav-trigger.nav-active span {
     width: 100%;
   }
 
-  .nav-trigger .is
+  .nav-trigger.nav-active {
+    -webkit-transform: rotate(225deg);
+    -ms-transform: rotate(225deg);
+    transform: rotate(225deg);
+  }
+
+  .nav-trigger.nav-active span:first-child{
+    -webkit-transform: rotate(90deg) translateX(0.00282em);
+    -ms-transform: rotate(90deg) translateX(0.00282em);
+    transform: rotate(90deg) translateX(0.00282em);
+  }
+  .nav-trigger.nav-active span:last-child{
+    width: 100%;
+    -webkit-transform: rotate(90deg) translateX(-0.00282em);
+    -ms-transform: rotate(90deg) translateX(-0.00282em);
+    transform: rotate(90deg) translateX(-0.00282em);
+  }
 </style>
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,13 +1,19 @@
 <template>
-  <nav-trigger></nav-trigger>
+  <nav-trigger :size=32 tint-color="336699" @click.native="clis()"></nav-trigger>
 </template>
 
 <script>
   import NavTrigger from '../components/widgets/NavTrigger.vue'
+
   export default {
     name: "Index",
     components: {
       NavTrigger,
+    },
+    methods: {
+      clis(){
+        console.log('test');
+      }
     }
   }
 </script>
